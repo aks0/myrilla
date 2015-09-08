61483b52046b93fc34461a954806a878e1cc1475
Akshay Mittal <akshaymittal10@gmail.com>
2015-09-07 22:20:33 -0700

    add SpotifyPlayerFragment and clean MainActivity

--- app/src/main/java/com/hissar/myrilla/app/MainActivity.java
+++ app/src/main/java/com/hissar/myrilla/app/MainActivity.java
@@ -6,24 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import com.parse.Parse;
 import com.parse.ParseObject;
-import com.spotify.sdk.android.authentication.AuthenticationClient;
-import com.spotify.sdk.android.authentication.AuthenticationRequest;
-import com.spotify.sdk.android.authentication.AuthenticationResponse;
-import com.spotify.sdk.android.player.*;
+import com.spotify.sdk.android.player.Spotify;
 
-public class MainActivity extends Activity implements
-    PlayerNotificationCallback, ConnectionStateCallback {
+public class MainActivity extends Activity {
 
-  private static final String SPOTIFY_CLIENT_ID = "417ecb31de084af881bab36ec17034df";
-  private static final String REDIRECT_URI = "dp-music://callback";
+  private static final String SPOTIFY_PLAYER_FRAGMENT_TAG = "spotify_player_fragment_tag";
 
   private static final String PARSE_CLIENT_ID = "pCpf1WSUDP2AWFU99nQA5bOR2Jjq71ZbfY34HSqM";
   private static final String PARSE_APPLICATION_ID = "fh1ToF8l7fGtAyX1lw3rJWtsPne19jrXAdmqEIXJ";
 
-  private static final int REQUEST_CODE = 1337;
-
-  private Player mPlayer;
   private MyrillaListFragment mMyrillaListFragment;
+  private SpotifyPlayerFragment mSpotifyPlayerFragment;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +24,11 @@ public class MainActivity extends Activity implements
     setContentView(R.layout.activity_main);
 
     hideOverlayFragment();
-    AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(
-        SPOTIFY_CLIENT_ID,
-        AuthenticationResponse.Type.TOKEN,
-        REDIRECT_URI);
-    builder.setScopes(new String[]{"user-read-private", "streaming"});
-    AuthenticationRequest request = builder.build();
+    testParse();
+    initSpotifyPlayerFragment();
+  }
 
-    AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
-
+  private void testParse() {
     // Enable Local Datastore.
     Parse.enableLocalDatastore(this);
 
@@ -51,35 +40,18 @@ public class MainActivity extends Activity implements
   }
 
   @Override
-  public void onResume() {
-    super.onResume();
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == SpotifyPlayerFragment.REQUEST_CODE_AUTHENTICATION) {
+      mSpotifyPlayerFragment.onActivityResult(requestCode, resultCode, data);
+      return;
+    }
+
+    super.onActivityResult(requestCode, resultCode, data);
   }
 
   @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-    super.onActivityResult(requestCode, resultCode, intent);
-
-    // Check if result comes from the correct activity
-    if (requestCode == REQUEST_CODE) {
-      AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
-      if (response.getType() == AuthenticationResponse.Type.TOKEN) {
-        Config playerConfig = new Config(this, response.getAccessToken(), SPOTIFY_CLIENT_ID);
-        mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
-          @Override
-          public void onInitialized(Player player) {
-            mPlayer.addConnectionStateCallback(MainActivity.this);
-            mPlayer.addPlayerNotificationCallback(MainActivity.this);
-            MainActivity.this.onPlayerInitialized();
-//            mPlayer.play("spotify:track:5J4ZkQpzMUFojo1CtAZYpn");
-          }
-
-          @Override
-          public void onError(Throwable throwable) {
-            Log.e("akshay", "Could not initialize player: " + throwable.getMessage());
-          }
-        });
-      }
-    }
+  public void onResume() {
+    super.onResume();
   }
 
   private void onPlayerInitialized() {
@@ -89,7 +61,7 @@ public class MainActivity extends Activity implements
         new MyrillaListFragment.Listener() {
           @Override
           public void onPlayTrack(SpotifyTrack spotifyTrack) {
-            mPlayer.play(spotifyTrack.uri);
+            mSpotifyPlayerFragment.playTrack(spotifyTrack.uri);
           }
         });
 
@@ -113,55 +85,6 @@ public class MainActivity extends Activity implements
   }
 
   @Override
-  public void onLoggedIn() {
-    Log.d("akshay", "User logged in");
-  }
-
-  @Override
-  public void onLoggedOut() {
-    Log.d("akshay", "User logged out");
-  }
-
-  @Override
-  public void onLoginFailed(Throwable error) {
-    Log.d("akshay", "Login failed");
-  }
-
-  @Override
-  public void onTemporaryError() {
-    Log.d("akshay", "Temporary error occurred");
-  }
-
-  @Override
-  public void onConnectionMessage(String message) {
-    Log.d("akshay", "Received connection message: " + message);
-  }
-
-  @Override
-  public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
-    Log.d("akshay", "Playback event received: " + eventType.name());
-    switch (eventType) {
-      case END_OF_CONTEXT:
-        mMyrillaListFragment.onTrackEndEvent();
-        break;
-
-      // Handle event type as necessary
-      default:
-        break;
-    }
-  }
-
-  @Override
-  public void onPlaybackError(ErrorType errorType, String errorDetails) {
-    Log.d("akshay", "Playback error received: " + errorType.name());
-    switch (errorType) {
-      // Handle error type as necessary
-      default:
-        break;
-    }
-  }
-
-  @Override
   protected void onDestroy() {
     Spotify.destroyPlayer(this);
     super.onDestroy();
@@ -174,5 +97,31 @@ public class MainActivity extends Activity implements
         .beginTransaction()
         .hide(myrillaOverlayFragment)
         .commit();
+  }
+
+  private void initSpotifyPlayerFragment() {
+    mSpotifyPlayerFragment = (SpotifyPlayerFragment) getFragmentManager()
+        .findFragmentByTag(SPOTIFY_PLAYER_FRAGMENT_TAG);
+    if (mSpotifyPlayerFragment == null) {
+      mSpotifyPlayerFragment = new SpotifyPlayerFragment();
+
+      getFragmentManager()
+          .beginTransaction()
+          .add(mSpotifyPlayerFragment, SPOTIFY_PLAYER_FRAGMENT_TAG)
+          .commit();
+    }
+
+    mSpotifyPlayerFragment.setListener(
+        new SpotifyPlayerFragment.Listener() {
+          @Override
+          public void onPlayerInitialized() {
+            MainActivity.this.onPlayerInitialized();
+          }
+
+          @Override
+          public void onTrackEnd() {
+            mMyrillaListFragment.onTrackEndEvent();
+          }
+        });
   }
 }
---
+++ app/src/main/java/com/hissar/myrilla/app/SpotifyPlayerFragment.java
@@ -0,0 +1,139 @@
+package com.hissar.myrilla.app;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.*;
+
+/**
+ * Controller fragment to manage the player and Spotify connection related actions.
+ */
+public class SpotifyPlayerFragment extends Fragment
+    implements PlayerNotificationCallback, ConnectionStateCallback {
+
+  public interface Listener {
+
+    /**
+     * Called when the spotify player {@link Player} has been initialized.
+     */
+    void onPlayerInitialized();
+
+    /**
+     * Called when the current playing track has ended.
+     */
+    void onTrackEnd();
+  }
+
+  public static final int REQUEST_CODE_AUTHENTICATION = 1337;
+
+  private static final String SPOTIFY_CLIENT_ID = "417ecb31de084af881bab36ec17034df";
+  private static final String REDIRECT_URI = "dp-music://callback";
+
+  private Listener mListener;
+  private Player mPlayer;
+
+  public void setListener(Listener listener) {
+    mListener = listener;
+  }
+
+  public void playTrack(String trackUri) {
+    mPlayer.play(trackUri);
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+
+    AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(
+        SPOTIFY_CLIENT_ID,
+        AuthenticationResponse.Type.TOKEN,
+        REDIRECT_URI);
+    builder.setScopes(new String[]{"user-read-private", "streaming"});
+    AuthenticationRequest request = builder.build();
+
+    AuthenticationClient.openLoginActivity(getActivity(), REQUEST_CODE_AUTHENTICATION, request);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode != REQUEST_CODE_AUTHENTICATION) {
+      super.onActivityResult(requestCode, resultCode, data);
+      return;
+    }
+
+    AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+    if (response.getType() != AuthenticationResponse.Type.TOKEN) {
+      return;
+    }
+
+    Config playerConfig = new Config(getActivity(), response.getAccessToken(), SPOTIFY_CLIENT_ID);
+    mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+      @Override
+      public void onInitialized(Player player) {
+        mPlayer.addConnectionStateCallback(SpotifyPlayerFragment.this);
+        mPlayer.addPlayerNotificationCallback(SpotifyPlayerFragment.this);
+        Log.d("akshay", "onInitialized()");
+        mListener.onPlayerInitialized();
+        // mPlayer.play("spotify:track:5J4ZkQpzMUFojo1CtAZYpn");
+      }
+
+      @Override
+      public void onError(Throwable throwable) {
+        Log.e("akshay", "Could not initialize player: " + throwable.getMessage());
+      }
+    });
+  }
+
+  @Override
+  public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+    Log.d("akshay", "Playback event received: " + eventType.name());
+    switch (eventType) {
+      case END_OF_CONTEXT:
+        mListener.onTrackEnd();
+        break;
+
+      // Handle event type as necessary
+      default:
+        break;
+    }
+  }
+
+  @Override
+  public void onPlaybackError(ErrorType errorType, String s) {
+    Log.d("akshay", "Playback error received: " + errorType.name());
+    switch (errorType) {
+      // Handle error type as necessary
+      default:
+        break;
+    }
+  }
+
+  @Override
+  public void onLoggedIn() {
+    Log.d("akshay", "User logged in");
+  }
+
+  @Override
+  public void onLoggedOut() {
+    Log.d("akshay", "User logged out");
+  }
+
+  @Override
+  public void onLoginFailed(Throwable error) {
+    Log.d("akshay", "Login failed");
+  }
+
+  @Override
+  public void onTemporaryError() {
+    Log.d("akshay", "Temporary error occurred");
+  }
+
+  @Override
+  public void onConnectionMessage(String message) {
+    Log.d("akshay", "Received connection message: " + message);
+  }
+}
